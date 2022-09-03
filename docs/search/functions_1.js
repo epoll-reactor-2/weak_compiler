96a89fb07c7ad7a3cb8d528aa6e4af498d30dbd2
epoll-reactor-2 <>
2022-09-03 13:58:41 +0200

    Add script for local environment tuning.

--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,10 +1,9 @@
 var searchData=
 [
-  ['checkifhavemoretokens_0',['CheckIfHaveMoreTokens',['../classweak_1_1frontEnd_1_1Parser.html#ab4c4f2990ce5a7924410bf10bc53d4c3',1,'weak::frontEnd::Parser']]],
-  ['closingquotecheck_1',['ClosingQuoteCheck',['../classweak_1_1frontEnd_1_1anonymous__namespace_02Lexer_8cpp_03_1_1LexStringLiteralCheck.html#a504fb5b7d89f81bd06dd4d335f364f0a',1,'weak::frontEnd::anonymous_namespace{Lexer.cpp}::LexStringLiteralCheck']]],
-  ['codegen_2',['CodeGen',['../classweak_1_1middleEnd_1_1CodeGen.html#a00299247d3165566a98f42359898bf5f',1,'weak::middleEnd::CodeGen']]],
-  ['commonwhilestmtvisit_3',['CommonWhileStmtVisit',['../classanonymous__namespace_02ASTPrettyPrint_8cpp_03_1_1ASTPrintVisitor.html#a13dee08691b621aa9de6854a79527482',1,'anonymous_namespace{ASTPrettyPrint.cpp}::ASTPrintVisitor']]],
-  ['compileerror_4',['CompileError',['../namespaceweak.html#a8fc005bbaa3fae23d9edbd2ff45d0237',1,'weak::CompileError()'],['../namespaceweak.html#aa5438354f7f3b961edc68238b1dd56b0',1,'weak::CompileError(unsigned LineNo, unsigned ColumnNo)']]],
-  ['compilewarning_5',['CompileWarning',['../namespaceweak.html#a42da0e444faf964525111a177a902859',1,'weak::CompileWarning()'],['../namespaceweak.html#a00712649da5b4e5011571afb7bdcfc97',1,'weak::CompileWarning(unsigned LineNo, unsigned ColumnNo)']]],
-  ['createcode_6',['CreateCode',['../classweak_1_1middleEnd_1_1CodeGen.html#a18ba470affced2071d27e883d3651366',1,'weak::middleEnd::CodeGen']]]
+  ['dolexicalanalysis_0',['DoLexicalAnalysis',['../compiler_8cpp.html#ab1ab0d3db72ae2d6c8fedb9c061a9f0d',1,'compiler.cpp']]],
+  ['dollvmcodegen_1',['DoLLVMCodeGen',['../compiler_8cpp.html#a34722ced4e1783683bbb5c3123c636b9',1,'compiler.cpp']]],
+  ['dosyntaxanalysis_2',['DoSyntaxAnalysis',['../compiler_8cpp.html#aa630ace3b86ef8ebc9f03cc6e57fd67b',1,'compiler.cpp']]],
+  ['dumpast_3',['DumpAST',['../compiler_8cpp.html#ac1cb3b4aa138f94ba116b452687ef3cf',1,'compiler.cpp']]],
+  ['dumplexemes_4',['DumpLexemes',['../compiler_8cpp.html#ae376b6cfb3a01619b7602daeeba23432',1,'compiler.cpp']]],
+  ['dumpllvmir_5',['DumpLLVMIR',['../compiler_8cpp.html#abccd8d9b5d09c07b52128a1d330300be',1,'compiler.cpp']]]
 ];
---
+++ environment/pre-commit.sh
@@ -0,0 +1,4 @@
+echo "Running pre-commit hook..."
+doxygen
+find lib -regextype posix-extended -regex ".*\.[hc]pp" | xargs clang-format -i
+git add --update
