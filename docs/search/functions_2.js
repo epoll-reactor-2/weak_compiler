26e96019313f6f402dbac8c6953059a16ae74a76
epoll-reactor <>
2022-09-03 13:58:41 +0200

    Add script for local environment tuning.

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['diagnostic_0',['Diagnostic',['../classanonymous__namespace_02Diagnostic_8cpp_03_1_1Diagnostic.html#a4771f424a6123a7b9f3e034792654f7a',1,'anonymous_namespace{Diagnostic.cpp}::Diagnostic::Diagnostic(enum DiagLevel TheLevel, unsigned LineNo, unsigned ColumnNo)'],['../classanonymous__namespace_02Diagnostic_8cpp_03_1_1Diagnostic.html#af7d393e537748d6bad90912839bb45f5',1,'anonymous_namespace{Diagnostic.cpp}::Diagnostic::Diagnostic(enum DiagLevel TheLevel)']]]
+  ['main_0',['main',['../compiler_8cpp.html#a6f32808498dbad5b4b48383c5e146ac4',1,'compiler.cpp']]]
 ];
--- docs/search/searchdata.js
+++ docs/search/searchdata.js
@@ -1,42 +1,21 @@
 var indexSectionsWithContent =
 {
-  0: "abcdefgilmnoprstuvwx~",
-  1: "acdloprstu",
-  2: "aw",
-  3: "acdlmpstuv",
-  4: "acdegilmnoprstuv~",
-  5: "abcdeilnoprstv",
-  6: "r",
-  7: "adtu",
-  8: "abcdefgilmnoprstvwx",
-  9: "c"
+  0: "bcdm",
+  1: "c",
+  2: "bdm"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "namespaces",
-  3: "files",
-  4: "functions",
-  5: "variables",
-  6: "typedefs",
-  7: "enums",
-  8: "enumvalues",
-  9: "related"
+  1: "files",
+  2: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes",
-  2: "Namespaces",
-  3: "Files",
-  4: "Functions",
-  5: "Variables",
-  6: "Typedefs",
-  7: "Enumerations",
-  8: "Enumerator",
-  9: "Friends"
+  1: "Files",
+  2: "Functions"
 };
 
