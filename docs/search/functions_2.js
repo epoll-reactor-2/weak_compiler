21d641e15acd971adb826435aca87d3b4103b8a5
epoll-reactor <>
2022-09-10 23:03:14 +0200

    Update Doxyfile.

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,4 +1,14 @@
 var searchData=
 [
-  ['main_0',['main',['../compiler_8cpp.html#a6f32808498dbad5b4b48383c5e146ac4',1,'compiler.cpp']]]
+  ['checkifhavemoretokens_0',['CheckIfHaveMoreTokens',['../classweak_1_1frontEnd_1_1Parser.html#ab4c4f2990ce5a7924410bf10bc53d4c3',1,'weak::frontEnd::Parser']]],
+  ['clearerrbuf_1',['ClearErrBuf',['../classDiagnostic.html#ac803c636bc0d58e1c13fa30bd8a79202',1,'Diagnostic']]],
+  ['closingquotecheck_2',['ClosingQuoteCheck',['../classweak_1_1frontEnd_1_1anonymous__namespace_02Lexer_8cpp_03_1_1LexStringLiteralCheck.html#a504fb5b7d89f81bd06dd4d335f364f0a',1,'weak::frontEnd::anonymous_namespace{Lexer.cpp}::LexStringLiteralCheck']]],
+  ['codegen_3',['CodeGen',['../classweak_1_1middleEnd_1_1CodeGen.html#a00299247d3165566a98f42359898bf5f',1,'weak::middleEnd::CodeGen']]],
+  ['commonwhilestmtvisit_4',['CommonWhileStmtVisit',['../classanonymous__namespace_02ASTDump_8cpp_03_1_1ASTDumpVisitor.html#a9bf190959d77dfd560d926c37b3ea6f9',1,'anonymous_namespace{ASTDump.cpp}::ASTDumpVisitor']]],
+  ['compileerror_5',['CompileError',['../namespaceweak.html#a8fc005bbaa3fae23d9edbd2ff45d0237',1,'weak::CompileError()'],['../namespaceweak.html#aa5438354f7f3b961edc68238b1dd56b0',1,'weak::CompileError(unsigned LineNo, unsigned ColumnNo)'],['../namespaceweak.html#a70ea12a9d8928d355c27d69ebe3f7f7b',1,'weak::CompileError(const weak::frontEnd::ASTNode *Node)']]],
+  ['compilewarning_6',['CompileWarning',['../namespaceweak.html#a42da0e444faf964525111a177a902859',1,'weak::CompileWarning()'],['../namespaceweak.html#a00712649da5b4e5011571afb7bdcfc97',1,'weak::CompileWarning(unsigned LineNo, unsigned ColumnNo)']]],
+  ['createcode_7',['CreateCode',['../classweak_1_1middleEnd_1_1CodeGen.html#ad8105f30ffb9e9b06b59a2fd6aa5d375',1,'weak::middleEnd::CodeGen']]],
+  ['createdataarray_8',['CreateDataArray',['../classanonymous__namespace_02CodeGen_8cpp_03_1_1StringLiteralBuilder.html#af69160174ce126146cad228d80d5ddf5',1,'anonymous_namespace{CodeGen.cpp}::StringLiteralBuilder']]],
+  ['createnullterminator_9',['CreateNullTerminator',['../classanonymous__namespace_02CodeGen_8cpp_03_1_1StringLiteralBuilder.html#a5a7e4b11c0c9de7d77020d3eb93dae9b',1,'anonymous_namespace{CodeGen.cpp}::StringLiteralBuilder']]],
+  ['createsignature_10',['CreateSignature',['../classanonymous__namespace_02CodeGen_8cpp_03_1_1FunctionBuilder.html#a8a712c0846b5d3f30dbdf5114591e7ef',1,'anonymous_namespace{CodeGen.cpp}::FunctionBuilder']]]
 ];
