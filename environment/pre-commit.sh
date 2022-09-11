765ab4df716c1b0cb585128e425bc15b7f06a1a7
epoll-reactor-2 <>
2022-09-11 11:47:19 +0200

    Add license comments reminder to pre-commit hook.

--- environment/pre-commit.sh
+++ environment/pre-commit.sh
@@ -1,4 +1,19 @@
 echo "Running pre-commit hook..."
-# doxygen
-find lib -regextype posix-extended -regex ".*\.[hc]pp" | xargs clang-format -i
-git add --update
+
+check_license_notes() {
+    for filename in `find lib -regextype posix-extended -regex ".*\.[hc]pp"`; do
+        case `cat $filename` in
+            "/*"*) true;;
+                *) echo "Expected license comment in $filename" && exit 1;;
+        esac
+    done
+}
+
+format() {
+    # doxygen
+    find lib -regextype posix-extended -regex ".*\.[hc]pp" | xargs clang-format -i
+    git add --update
+}
+
+check_license_notes
+format
