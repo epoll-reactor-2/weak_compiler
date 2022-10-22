1d9e4452a35c1c8ac136303ae95b7bbf5f2aa6f3
epoll-reactor-2 <>
2022-10-22 19:21:25 +0200

    Update doxyfile.

--- docs/search/classes_2.js
+++ docs/search/classes_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['declrecord_0',['DeclRecord',['../structweak_1_1DeclsStorage_1_1DeclRecord.html',1,'weak::DeclsStorage']]],
-  ['declsstorage_1',['DeclsStorage',['../classweak_1_1DeclsStorage.html',1,'weak']]],
-  ['diagnostic_2',['Diagnostic',['../classDiagnostic.html',1,'']]]
+  ['declsstorage_1',['DeclsStorage',['../classweak_1_1DeclsStorage.html',1,'weak']]]
 ];
