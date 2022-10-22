0a2795a10a06743efb5fe4592118f53e877ff6d0
epoll-reactor <>
2022-10-22 19:21:25 +0200

    Update doxyfile.

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -5,6 +5,5 @@ var searchData=
   ['declrecord_2',['DeclRecord',['../structweak_1_1DeclsStorage_1_1DeclRecord.html',1,'weak::DeclsStorage']]],
   ['declsstorage_3',['DeclsStorage',['../classweak_1_1DeclsStorage.html',1,'weak']]],
   ['declstorage_4',['DeclStorage',['../classweak_1_1CodeGen.html#a298d3e56ef5442f9172eb605f1d87982',1,'weak::CodeGen']]],
-  ['depth_5',['Depth',['../structweak_1_1DeclsStorage_1_1DeclRecord.html#ac716229fb24bc570b31743886469996c',1,'weak::DeclsStorage::DeclRecord']]],
-  ['diagnostic_6',['Diagnostic',['../classDiagnostic.html',1,'']]]
+  ['depth_5',['Depth',['../structweak_1_1DeclsStorage_1_1DeclRecord.html#ac716229fb24bc570b31743886469996c',1,'weak::DeclsStorage::DeclRecord']]]
 ];
