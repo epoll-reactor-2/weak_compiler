3b9e91151a75fce15aee375c4c1a637e674db790
epoll-reactor <>
2022-10-23 19:56:17 +0200

    Remove std::variant visits stub.

--- docs/search/classes_4.js
+++ docs/search/classes_4.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['ostreamraii_0',['OstreamRAII',['../structweak_1_1OstreamRAII.html',1,'weak']]],
-  ['overload_1',['Overload',['../structweak_1_1Overload.html',1,'weak']]]
+  ['ostreamraii_0',['OstreamRAII',['../structweak_1_1OstreamRAII.html',1,'weak']]]
 ];
