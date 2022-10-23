a445ee458e1555409aac530e0cab5f1bef8bc594
epoll-reactor-2 <>
2022-10-23 19:56:17 +0200

    Remove std::variant visits stub.

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['objectfilepath_0',['ObjectFilePath',['../classweak_1_1TargetCodeBuilder.html#aec74ccb08936d4947a174c665cc49003',1,'weak::TargetCodeBuilder']]],
-  ['ostreamraii_1',['OstreamRAII',['../structweak_1_1OstreamRAII.html',1,'weak']]],
-  ['overload_2',['Overload',['../structweak_1_1Overload.html',1,'weak']]]
+  ['ostreamraii_1',['OstreamRAII',['../structweak_1_1OstreamRAII.html',1,'weak']]]
 ];
