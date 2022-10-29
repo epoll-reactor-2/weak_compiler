51b92c630380316b461b6a9ca75f63d466f81375
epoll-reactor-2 <>
2022-10-29 16:56:47 +0200

    Make header for all AST nodes.

---
+++ lib/include/FrontEnd/AST/AST.h
@@ -0,0 +1,28 @@
+#ifndef WEAK_COMPILER_FRONTEND_AST_AST_H
+#define WEAK_COMPILER_FRONTEND_AST_AST_H
+
+#include "FrontEnd/AST/ASTArrayAccess.h"
+#include "FrontEnd/AST/ASTArrayDecl.h"
+#include "FrontEnd/AST/ASTBinaryOperator.h"
+#include "FrontEnd/AST/ASTBooleanLiteral.h"
+#include "FrontEnd/AST/ASTBreakStmt.h"
+#include "FrontEnd/AST/ASTCharLiteral.h"
+#include "FrontEnd/AST/ASTCompoundStmt.h"
+#include "FrontEnd/AST/ASTContinueStmt.h"
+#include "FrontEnd/AST/ASTDoWhileStmt.h"
+#include "FrontEnd/AST/ASTFloatingPointLiteral.h"
+#include "FrontEnd/AST/ASTForStmt.h"
+#include "FrontEnd/AST/ASTFunctionCall.h"
+#include "FrontEnd/AST/ASTFunctionDecl.h"
+#include "FrontEnd/AST/ASTFunctionPrototype.h"
+#include "FrontEnd/AST/ASTIfStmt.h"
+#include "FrontEnd/AST/ASTIntegerLiteral.h"
+#include "FrontEnd/AST/ASTReturnStmt.h"
+#include "FrontEnd/AST/ASTStringLiteral.h"
+#include "FrontEnd/AST/ASTStructDecl.h"
+#include "FrontEnd/AST/ASTSymbol.h"
+#include "FrontEnd/AST/ASTUnaryOperator.h"
+#include "FrontEnd/AST/ASTVarDecl.h"
+#include "FrontEnd/AST/ASTWhileStmt.h"
+
+#endif // WEAK_COMPILER_FRONTEND_AST_AST_H
--- lib/src/FrontEnd/AST/ASTDump.cpp
+++ lib/src/FrontEnd/AST/ASTDump.cpp
@@ -5,31 +5,8 @@
  */
 
 #include "FrontEnd/AST/ASTDump.h"
-#include "FrontEnd/AST/ASTArrayAccess.h"
-#include "FrontEnd/AST/ASTArrayDecl.h"
-#include "FrontEnd/AST/ASTBinaryOperator.h"
-#include "FrontEnd/AST/ASTBooleanLiteral.h"
-#include "FrontEnd/AST/ASTBreakStmt.h"
-#include "FrontEnd/AST/ASTCharLiteral.h"
-#include "FrontEnd/AST/ASTCompoundStmt.h"
-#include "FrontEnd/AST/ASTContinueStmt.h"
-#include "FrontEnd/AST/ASTDoWhileStmt.h"
-#include "FrontEnd/AST/ASTFloatingPointLiteral.h"
-#include "FrontEnd/AST/ASTForStmt.h"
-#include "FrontEnd/AST/ASTFunctionCall.h"
-#include "FrontEnd/AST/ASTFunctionDecl.h"
-#include "FrontEnd/AST/ASTFunctionPrototype.h"
-#include "FrontEnd/AST/ASTIfStmt.h"
-#include "FrontEnd/AST/ASTIntegerLiteral.h"
-#include "FrontEnd/AST/ASTNode.h"
-#include "FrontEnd/AST/ASTReturnStmt.h"
-#include "FrontEnd/AST/ASTStringLiteral.h"
-#include "FrontEnd/AST/ASTStructDecl.h"
-#include "FrontEnd/AST/ASTSymbol.h"
-#include "FrontEnd/AST/ASTUnaryOperator.h"
-#include "FrontEnd/AST/ASTVarDecl.h"
+#include "FrontEnd/AST/AST.h"
 #include "FrontEnd/AST/ASTVisitor.h"
-#include "FrontEnd/AST/ASTWhileStmt.h"
 
 template <typename It>
 static std::string IntegerRangeToString(It Begin, It End) {
