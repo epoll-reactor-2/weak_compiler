f0463d3aa3492215590b2733d2dd619c7d60a67c
epoll-reactor <>
2022-11-01 23:04:17 +0100

    Add built-in LLVM optimization passes.

---
+++ lib/include/MiddleEnd/Optimizers/Optimizers.h
@@ -0,0 +1,24 @@
+/* Optimizations.h - Optimization passes for LLVM IR.
+ * Copyright (C) 2022 epoll-reactor <glibcxx.chrono@gmail.com>
+ *
+ * This file is distributed under the MIT license.
+ */
+
+#ifndef WEAK_COMPILER_MIDDLE_END_OPTIMIZERS_H
+#define WEAK_COMPILER_MIDDLE_END_OPTIMIZERS_H
+
+#include "llvm/IR/Module.h"
+
+/// Global due to clEnumVal use.
+enum WeakOptimizationLevel { O0 = 0, O1, O2, O3 };
+
+namespace weak {
+
+/// Perform built-in LLVM optimizations of given level.
+///
+/// \todo Implement own optimizations.
+void RunBuiltinLLVMOptimizationPass(llvm::Module &M, WeakOptimizationLevel OptLvl);
+
+} // namespace weak
+
+#endif // WEAK_COMPILER_MIDDLE_END_OPTIMIZERS_H
