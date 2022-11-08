b6986c767efa817ff6a7421ca6ba2c4e096a162e
epoll-reactor <>
2022-11-08 16:22:12 +0100

    Remove dead code analysis stub.
    I don't know if it possible to implement this at current
    stage.

--- docs/search/classes_2.js
+++ docs/search/classes_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['deadcodeanalysis_0',['DeadCodeAnalysis',['../classweak_1_1DeadCodeAnalysis.html',1,'weak']]],
-  ['declaration_1',['Declaration',['../structweak_1_1ASTStorage_1_1Declaration.html',1,'weak::ASTStorage']]],
-  ['declrecord_2',['DeclRecord',['../structweak_1_1Storage_1_1DeclRecord.html',1,'weak::Storage']]]
+  ['declaration_0',['Declaration',['../structweak_1_1ASTStorage_1_1Declaration.html',1,'weak::ASTStorage']]],
+  ['declrecord_1',['DeclRecord',['../structweak_1_1Storage_1_1DeclRecord.html',1,'weak::Storage']]]
 ];
