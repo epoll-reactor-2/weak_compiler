5e668291bebf7dc964a71beba182dfd6bc99f3a3
epoll-reactor-2 <>
2022-11-09 22:11:21 +0100

    Update pre-commit hook.
    Before this moment and after changing header extension from
    .hpp to .h, hook wasn't applied to headers.

--- environment/pre-commit.sh
+++ environment/pre-commit.sh
@@ -1,7 +1,7 @@
 echo "Running pre-commit hook..."
 
 check_license_notes() {
-    for filename in `find lib -regextype posix-extended -regex ".*\.[hc]pp"`; do
+    for filename in `find lib -regextype posix-extended -regex ".*\.(h|cpp)"`; do
         case `cat $filename` in
             "/*"*) true;;
                 *) echo "Expected license comment in $filename" && exit 1;;
