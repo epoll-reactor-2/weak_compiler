e5c1f991f3ce8b90c2cf81def306f99013163dd4
epoll-reactor <>
2022-12-19 11:53:39 +0100

    Add copyright note to fuzzer.

--- tests/Utility/Fuzzer.cpp
+++ tests/Utility/Fuzzer.cpp
@@ -1,6 +1,8 @@
-/*
-
-*/
+/* Fuzzer.cpp - Valid weak language code programs generator and tester.
+ * Copyright (C) 2022 epoll-reactor <glibcxx.chrono@gmail.com>
+ *
+ * This file is distributed under the MIT license.
+ */
 
 #include "FrontEnd/Lex/Lexer.h"
 #include "FrontEnd/Parse/Parser.h"
@@ -48,16 +50,16 @@ static const char *DataTypes[] = {
 };
 
 static char LettersSet[] = {
-    'A','B','C','D','E','F',
-    'G','H','I','J','K',
-    'L','M','N','O','P',
-    'Q','R','S','T','U',
-    'V','W','X','Y','Z',
-    'a','b','c','d','e','f',
-    'g','h','i','j','k',
-    'l','m','n','o','p',
-    'q','r','s','t','u',
-    'v','w','x','y','z'
+  'A','B','C','D','E','F',
+  'G','H','I','J','K',
+  'L','M','N','O','P',
+  'Q','R','S','T','U',
+  'V','W','X','Y','Z',
+  'a','b','c','d','e','f',
+  'g','h','i','j','k',
+  'l','m','n','o','p',
+  'q','r','s','t','u',
+  'v','w','x','y','z'
 };
 
 std::default_random_engine RandomEngine(std::random_device{}());
