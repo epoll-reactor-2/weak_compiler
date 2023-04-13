1d389132549d3be80a263eebc8caa1f8e7200166
epoll-reactor-2 <>
2023-04-13 10:07:03 +0200

    IR graph stub.

---
+++ lib/middle_end/ir_graph.c
@@ -0,0 +1,7 @@
+/* ir_graph.c - Functions to build graph from IR.
+ * Copyright (C) 2023 epoll-reactor <glibcxx.chrono@gmail.com>
+ *
+ * This file is distributed under the MIT license.
+ */
+
+#include "middle_end/ir_graph.h"
---
+++ lib/middle_end/ir_graph.h
@@ -0,0 +1,20 @@
+/* ir_graph.h - Functions to build graph from IR.
+ * Copyright (C) 2023 epoll-reactor <glibcxx.chrono@gmail.com>
+ *
+ * This file is distributed under the MIT license.
+ */
+
+#ifndef WEAK_COMPILER_MIDDLE_END_IR_GRAPH_H
+#define WEAK_COMPILER_MIDDLE_END_IR_GRAPH_H
+
+/// \todo: How to represent graph?
+///   1:
+///     form:  Adjacency matrix
+///     usage: SSA form computing using this algorithm:
+///            https://www.sciencedirect.com/science/article/pii/S1571066107005324
+///
+///   2:
+///     form:  Adjacency list
+///     usage: ...
+
+#endif // WEAK_COMPILER_MIDDLE_END_IR_GRAPH_H
