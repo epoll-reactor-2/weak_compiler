1c8eb37c4c3df1ded5fe5fbaec61097ba72f376d
epoll-reactor-2 <>
2023-06-24 13:43:58 +0000

    Add dominator tree algorithm.

--- lib/middle_end/ir_graph.c
+++ lib/middle_end/ir_graph.c
@@ -5,12 +5,20 @@
  */
 
 #include "middle_end/ir_graph.h"
+#include "middle_end/ir_dump.h"
 #include "middle_end/ir.h"
 #include "util/alloc.h"
 #include "util/compiler.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 static __weak_unused void ir_graph_dfs(struct ir_graph *graph);
+static __weak_unused void ir_graph_build_dom_tree(
+    struct ir_graph *graph,
+    struct ir_graph *out_dom_tree,
+    struct ir_node  *decls
+);
 
 static __weak_unused inline void ir_graph_set_at(
     struct ir_graph *graph,
@@ -82,7 +90,15 @@ struct ir_graph ir_graph_init(struct ir *ir)
 
     ir_graph_build_matrix(decl->body, &graph);
 
-    ir_graph_dfs(&graph);
+    // ir_graph_dfs(&graph);
+
+    struct ir_graph dom_tree = {
+        .bytes_size = matrix_size,
+        .cols_count = decl->body_size,
+        .adj_matrix = weak_calloc(matrix_size, sizeof (bool))
+    };
+
+    ir_graph_build_dom_tree(&graph, &dom_tree, ir->decls);
 
     return graph;
 }
@@ -136,6 +152,67 @@ static void __ir_graph_dfs(
             }
         }
     }
+}
+
+/// \todo: Compute dominance not by node indices, but by
+///        variable indices inside each node.
+///
+///        +--------------+  This should be root of all
+///        | store %1, %2 |  nodes, that contain %1 and %2
+///        +--------------+
+static __weak_unused void ir_graph_build_dom_tree(
+    struct ir_graph *graph,
+    struct ir_graph *out_dom_tree,
+    struct ir_node  *decls
+) {
+    uint64_t  siz  = graph->cols_count;
+     int64_t *doms = weak_calloc(siz, sizeof (int64_t));
+
+    for (uint64_t i = 0; i < siz; ++i) {
+        doms[i] = -1; /// Undefined.
+
+        for (uint64_t j = 0; j < siz; ++j) {
+            if (ir_graph_get_at(graph, i, j)) {
+                if (doms[i] == -1) {
+                    doms[i] = j;
+                } else {
+                    int idom = doms[i];
+                    while (idom != doms[idom] && doms[idom] != -1)
+                        idom = doms[idom];
+                    int intersect = idom;
+                    idom = doms[i];
+                    while (idom != intersect && idom != -1) {
+                        doms[i] = intersect;
+                        idom = doms[intersect];
+                        while (idom != intersect && idom != -1) {
+                            idom = doms[idom];
+                        }
+                        intersect = idom;
+                    }
+                }
+            }
+        }
+    }
+
+    for (uint64_t i = 1; i < siz; ++i)
+        ir_graph_set_at(out_dom_tree, i, doms[i], 1);
+
+    struct ir_func_decl *func = (struct ir_func_decl *) decls[0].ir;
+
+    FILE *stream_1 = fopen("/tmp/graph_1.dot", "w");
+    FILE *stream_2 = fopen("/tmp/graph_2.dot", "w");
+
+    // printf("Dom tree (source):\n");
+    ir_dump_graph_dot(stream_1, graph->adj_matrix, func->body_size, func->body);
+    // printf("Dom tree (output):\n");
+    ir_dump_graph_dot(stream_2, out_dom_tree->adj_matrix, func->body_size, func->body);
+
+    fflush(stream_1);
+    fflush(stream_2);
+    fclose(stream_1);
+    fclose(stream_2);
+
+    weak_free(doms);
 }
 
 /*
