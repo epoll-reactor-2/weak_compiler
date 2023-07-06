8601b12c6f7d214eac6a4b0ff1f20ef865638ad9
epoll-reactor-2 <>
2023-07-06 21:28:02 +0000

    Fold binary expressions chains to immediate value.

--- lib/middle_end/opt/fold.c
+++ lib/middle_end/opt/fold.c
@@ -12,6 +12,10 @@
 #include <string.h>
 #include <assert.h>
 
+/// \todo: *) Constants mapping for all types.
+///        *) Remove dead store and alloca.
+///        *) Refactor. This code was written in 30 minutes.
+///           Requiers some meditation process.
 static struct ir_node last_folded;
 static hashmap_t      consts_mapping;
 
@@ -21,6 +25,33 @@ static void consts_mapping_init()
         hashmap_destroy(&consts_mapping);
     }
     hashmap_init(&consts_mapping, 512);
+}
+
+__weak_really_inline static void consts_mapping_add(uint64_t idx, uint64_t value)
+{
+    hashmap_put(&consts_mapping, idx, value);
+    printf("Consts mapping: add idx:%ld, value:%ld\n", idx, value);
+}
+
+__weak_really_inline static uint64_t consts_mapping_get(uint64_t idx)
+{
+    uint64_t got = hashmap_get(&consts_mapping, idx);
+    printf("Consts mapping: get value of idx:%ld -> %ld\n", idx, got);
+    return got;
+}
+
+__weak_really_inline static void consts_mapping_update(uint64_t idx, uint64_t value)
+{   
+    hashmap_remove(&consts_mapping, idx);
+    hashmap_put(&consts_mapping, idx, value);
+    printf("Consts mapping: update idx:%ld, value:%ld\n", idx, value);
+}
+
+__weak_really_inline static bool consts_mapping_is_const(uint64_t idx)
+{
+    uint64_t got = hashmap_get(&consts_mapping, idx);
+    printf("Consts mapping: is_const? idx:%ld -> %s\n", idx, (got != 0) ? "yes" : "no");
+    return got != 0;
 }
 
 __weak_really_inline static bool fold_booleans(enum token_type op, bool l, bool r)
@@ -118,14 +149,65 @@ static void fold_node(struct ir_node *ir)
         break;
     }
     case IR_STORE: {
-        struct ir_store *l = ir->ir;
-        switch (l->type) {
+        struct ir_store *stmt = ir->ir;
+        switch (stmt->type) {
         case IR_STORE_BIN: {
-            fold_node(&l->body);
-            ir_node_cleanup(l->body);
-            l->body = last_folded;
-            l->type = IR_STORE_IMM;
+            struct ir_bin *bin = stmt->body.ir;
+
+            if (bin->lhs.type == IR_IMM &&
+                bin->rhs.type == IR_IMM) {
+                fold_node(&stmt->body);
+                ir_node_cleanup(stmt->body);
+                stmt->body = last_folded;
+                stmt->type = IR_STORE_IMM;
+
+                struct ir_imm *imm = stmt->body.ir;
+                consts_mapping_add(stmt->idx, imm->imm_int);
+            }
+
+            if ((bin->lhs.type == IR_SYM &&
+                 bin->rhs.type == IR_IMM
+                ) || (
+                 bin->lhs.type == IR_SYM &&
+                 bin->rhs.type == IR_SYM
+                )
+            ) {
+                struct ir_sym *sym = bin->lhs.ir;
+
+                if (consts_mapping_is_const(sym->idx)) {
+                    fold_node(&stmt->body);
+                    ir_node_cleanup(stmt->body);
+                    stmt->body = last_folded;
+                    stmt->type = IR_STORE_IMM;
+
+                    struct ir_imm *imm = stmt->body.ir;
+                    consts_mapping_update(stmt->idx, imm->imm_int);
+                }
+            }
+
             break;
+        }
+        case IR_STORE_VAR: {
+            struct ir_sym *sym = stmt->body.ir;
+
+            if (consts_mapping_is_const(sym->idx)) {
+                /// \todo: Get any type (ints, floats, etc);
+                uint64_t imm = consts_mapping_get(sym->idx);
+
+                ir_node_cleanup(stmt->body);
+                stmt->body = ir_imm_int_init(imm);
+                stmt->type = IR_STORE_IMM;
+
+                consts_mapping_update(stmt->idx, imm);
+            }
+            break;
+        }
+        case IR_STORE_IMM: {
+            struct ir_imm *imm = stmt->body.ir;
+
+            if (consts_mapping_is_const(stmt->idx)) {
+                consts_mapping_update(stmt->idx, imm->imm_int);
+            }
         }
         default:
             break;
@@ -136,6 +218,8 @@ static void fold_node(struct ir_node *ir)
         struct ir_bin *l = ir->ir;
         if (l->lhs.type == IR_IMM &&
             l->rhs.type == IR_IMM) {
+            printf("Both immediates\n");
+
             struct ir_imm *l_imm = l->lhs.ir;
             struct ir_imm *r_imm = l->rhs.ir;
             assert(l_imm->type == r_imm->type);
@@ -144,14 +228,74 @@ static void fold_node(struct ir_node *ir)
                 last_folded = ir_imm_bool_init(fold_booleans(l->op, l_imm->imm_bool, r_imm->imm_bool));
                 break;
             case IMM_CHAR:
-                last_folded = ir_imm_char_init(fold_ints(l->op, l_imm->imm_bool, r_imm->imm_bool));
+                last_folded = ir_imm_char_init(fold_ints(l->op, l_imm->imm_char, r_imm->imm_char));
                 break;
             case IMM_FLOAT:
-                last_folded = ir_imm_float_init(fold_floats(l->op, l_imm->imm_bool, r_imm->imm_bool));
+                last_folded = ir_imm_float_init(fold_floats(l->op, l_imm->imm_float, r_imm->imm_float));
                 break;
             case IMM_INT:
-                last_folded = ir_imm_int_init(fold_ints(l->op, l_imm->imm_bool, r_imm->imm_bool));
+                last_folded = ir_imm_int_init(fold_ints(l->op, l_imm->imm_int, r_imm->imm_int));
                 break;
+            }
+        }
+
+        if (l->lhs.type == IR_SYM &&
+            l->rhs.type == IR_IMM) {
+            printf("Symbol and immediate\n");
+
+            struct ir_sym *sym = l->lhs.ir;
+            struct ir_imm *imm = l->rhs.ir;
+
+            if (consts_mapping_is_const(sym->idx)) {
+                uint64_t imm_val = consts_mapping_get(sym->idx);
+
+                switch (imm->type) {
+                case IMM_BOOL:
+                    last_folded = ir_imm_bool_init(fold_booleans(l->op, imm_val, imm->imm_bool));
+                    break;
+                case IMM_CHAR:
+                    last_folded = ir_imm_char_init(fold_ints(l->op, imm_val, imm->imm_char));
+                    break;
+                case IMM_FLOAT:
+                    last_folded = ir_imm_float_init(fold_floats(l->op, imm_val, imm->imm_float));
+                    break;
+                case IMM_INT:
+                    last_folded = ir_imm_int_init(fold_ints(l->op, imm_val, imm->imm_int));
+                    break;
+                }
+            }
+        }
+
+        if (l->lhs.type == IR_SYM &&
+            l->rhs.type == IR_SYM) {
+            struct ir_sym *sym_lhs = l->lhs.ir;
+            struct ir_sym *sym_rhs = l->rhs.ir;
+
+            printf("Both symbols\n");
+
+            bool lhs_const = consts_mapping_is_const(sym_lhs->idx);
+            bool rhs_const = consts_mapping_is_const(sym_rhs->idx);
+
+            if (lhs_const && rhs_const) {
+                uint64_t lhs_imm = consts_mapping_get(sym_lhs->idx);
+                uint64_t rhs_imm = consts_mapping_get(sym_rhs->idx);
+
+                /// Note: LHS and RHS types are the same.
+                ///       IR generator post-condition.
+                switch (IMM_INT) { /// Placeholder.
+                case IMM_BOOL:
+                    last_folded = ir_imm_bool_init(fold_booleans(l->op, lhs_imm, rhs_imm));
+                    break;
+                case IMM_CHAR:
+                    last_folded = ir_imm_char_init(fold_ints(l->op, lhs_imm, rhs_imm));
+                    break;
+                case IMM_FLOAT:
+                    last_folded = ir_imm_float_init(fold_floats(l->op, lhs_imm, rhs_imm));
+                    break;
+                case IMM_INT:
+                    last_folded = ir_imm_int_init(fold_ints(l->op, lhs_imm, rhs_imm));
+                    break;
+                }
             }
         }
         break;
@@ -163,8 +307,18 @@ static void fold_node(struct ir_node *ir)
     }
     case IR_RET:
     case IR_RET_VOID: {
-        struct ir_ret *l = ir->ir;
-        (void) l;
+        struct ir_ret *stmt = ir->ir;
+        if (!stmt->is_void) {
+            struct ir_sym *sym = stmt->op.ir;
+
+            if (consts_mapping_is_const(sym->idx)) {
+                uint64_t imm = consts_mapping_get(sym->idx);
+
+                /// \todo: Immediate emit for all types.
+                ir_node_cleanup(stmt->op);
+                stmt->op = ir_imm_int_init(imm);
+            }
+        }
         break;
     }
     case IR_MEMBER:
@@ -183,27 +337,6 @@ static void fold(struct ir_func_decl *decl)
 {
     for (uint64_t i = 0; i < decl->body_size; ++i) {
         struct ir_node *node = &decl->body[i];
-        /// \todo: Fold binary expressions chain to one
-        ///        instructions.
-        ///
-        ///        1 + 2 + 3 ->
-        ///
-        ///        alloca int %0
-        ///        store %0 2 + 3
-        ///        alloca int %1
-        ///        store %1 %0 + 1
-        ///
-        ///        ->
-        ///
-        ///        alloca int %0    ... Remove everything below
-        ///        store %0 5
-        ///
-        ///        Properties:
-        ///          if (is_const(%0)) ...
-        ///
-        ///        If there we store this way, maybe
-        ///        the rest of the job will do
-        ///        constant propagation algorithm?..
         fold_node(node);
    }
 }
