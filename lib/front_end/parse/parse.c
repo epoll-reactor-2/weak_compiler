3a766492c84bd84855e50ab825bd362feeea7d4d
epoll-reactor-2 <>
2023-09-11 19:51:07 +0000

    Get rid of boilerplate (const pointers) in parser.

--- lib/front_end/parse/parse.c
+++ lib/front_end/parse/parse.c
@@ -16,6 +16,10 @@
 
 typedef vector_t(struct ast_node *) ast_array_t;
 
+static struct token *tok_begin;
+static struct token *tok_end;
+static uint32_t      loops_depth = 0;
+
 static enum data_type tok_to_data_type(enum token_type t)
 {
     switch (t) {
@@ -33,16 +37,12 @@ static enum data_type tok_to_data_type(enum token_type t)
     }
 }
 
-static const struct token *tok_begin;
-static const struct token *tok_end;
-static uint32_t            loops_depth = 0;
-
-static const struct token *peek_current()
+static struct token *peek_current()
 {
     return tok_begin;
 }
 
-static const struct token *peek_next()
+static struct token *peek_next()
 {
     // printf(
         // "Peek next: %s\n",
@@ -53,9 +53,9 @@ static const struct token *peek_next()
     return tok_begin++;
 }
 
-const struct token *require_token(enum token_type t)
+struct token *require_token(enum token_type t)
 {
-    const struct token *curr_tok = peek_current();
+    struct token *curr_tok = peek_current();
 
     if (curr_tok->type != t)
         weak_compile_error(
@@ -69,7 +69,7 @@ const struct token *require_token(enum token_type t)
     return curr_tok;
 }
 
-const struct token *require_char(char c)
+struct token *require_char(char c)
 {
     return require_token(tok_char_to_tok(c));
 }
@@ -113,16 +113,16 @@ static struct ast_node *parse_function_call();
 struct ast_node *parse(const struct token *begin, const struct token *end)
 {
     tok_begin = (struct token *) begin;
-    tok_end = (struct token *) end;
+    tok_end   = (struct token *) end;
 
     typedef vector_t(struct ast_node *) stmts_t;
 
     stmts_t global_stmts = {0};
-    const struct token *curr_tok = NULL;
+    struct token *curr = NULL;
 
     while (tok_begin < tok_end) {
-        curr_tok = peek_current();
-        switch (curr_tok->type) {
+        curr = peek_current();
+        switch (curr->type) {
         case TOK_STRUCT:
             vector_push_back(global_stmts, parse_struct_decl());
             break;
@@ -135,10 +135,10 @@ struct ast_node *parse(const struct token *begin, const struct token *end)
             break;
         default:
             weak_compile_error(
-                curr_tok->line_no,
-                curr_tok->col_no,
+                curr->line_no,
+                curr->col_no,
                 "Unexpected token in global context: %s\n",
-                tok_to_string(curr_tok->type)
+                tok_to_string(curr->type)
             );
         }
     }
@@ -161,9 +161,9 @@ struct localized_data_type {
 
 static struct localized_data_type parse_type()
 {
-    const struct token *curr_tok = peek_next();
+    struct token *t = peek_next();
 
-    switch (curr_tok->type) {
+    switch (t->type) {
     case TOK_INT:
     case TOK_FLOAT:
     case TOK_CHAR:
@@ -176,42 +176,42 @@ static struct localized_data_type parse_type()
         }
 
         struct localized_data_type dt = {
-            .data_type       = tok_to_data_type(curr_tok->type),
-            .type_name       = (curr_tok->type == TOK_SYMBOL)
-                                 ? strdup(curr_tok->data)
+            .data_type       = tok_to_data_type(t->type),
+            .type_name       = (t->type == TOK_SYMBOL)
+                                 ? strdup(t->data)
                                  : NULL,
             .indirection_lvl = indirection_lvl,
-            .line_no         = curr_tok->line_no,
-            .col_no          = curr_tok->col_no
+            .line_no         = t->line_no,
+            .col_no          = t->col_no
         };
 
         return dt;
     }
     default:
         weak_compile_error(
-            curr_tok->line_no,
-            curr_tok->col_no,
+            t->line_no,
+            t->col_no,
             "Data type expected, got %s",
-            tok_to_string(curr_tok->type)
+            tok_to_string(t->type)
         );
     }
 }
 
 static struct localized_data_type parse_return_type()
 {
-    const struct token *curr_tok = peek_current();
+    struct token *t = peek_current();
 
-    if (curr_tok->type != TOK_VOID)
+    if (t->type != TOK_VOID)
         return parse_type();
 
     peek_next();
 
     struct localized_data_type dt = {
-        .data_type       = tok_to_data_type(curr_tok->type),
+        .data_type       = tok_to_data_type(t->type),
         .type_name       = NULL,
         .indirection_lvl = 0,
-        .line_no         = curr_tok->line_no,
-        .col_no          = curr_tok->col_no
+        .line_no         = t->line_no,
+        .col_no          = t->col_no
     };
 
     return dt;
@@ -220,7 +220,7 @@ static struct localized_data_type parse_return_type()
 static struct ast_node *parse_array_decl()
 {
     struct localized_data_type dt = parse_type();
-    const struct token *var_name = peek_next();
+    struct token *var_name = peek_next();
 
     if (var_name->type != TOK_SYMBOL)
         weak_compile_error(
@@ -272,10 +272,11 @@ static struct ast_node *parse_array_decl()
 
 static struct ast_node *parse_decl_without_initializer()
 {
-    const struct token *ptr = peek_current();
-    struct localized_data_type dt = parse_type();
-    bool is_array = tok_is((tok_begin + 1), '[');
-    ptrdiff_t offset = tok_begin - ptr;
+    struct token   *ptr      = peek_current();
+    struct localized_data_type
+                    dt       = parse_type();
+    bool            is_array = tok_is((tok_begin + 1), '[');
+    ptrdiff_t       offset   = tok_begin - ptr;
 
     tok_begin -= offset;
 
@@ -308,7 +309,7 @@ static struct ast_node *parse_decl_without_initializer()
 static struct ast_node *parse_var_decl_without_initializer()
 {
     struct localized_data_type dt = parse_type();
-    const struct token *var_name = require_token(TOK_SYMBOL);
+    struct token *var_name = require_token(TOK_SYMBOL);
 
     return ast_var_decl_init(
         dt.data_type,
@@ -324,7 +325,7 @@ static struct ast_node *parse_var_decl_without_initializer()
 static struct ast_node *parse_var_decl()
 {
     struct localized_data_type dt = parse_type();
-    const struct token *var_name = peek_next();
+    struct token *var_name = peek_next();
 
     if (var_name->type != TOK_SYMBOL)
         weak_compile_error(
@@ -333,7 +334,7 @@ static struct ast_node *parse_var_decl()
             "Variable name expected"
         );
 
-    const struct token *operator = peek_next();
+    struct token *operator = peek_next();
 
     if (tok_is(operator, '='))
         return ast_var_decl_init(
@@ -372,7 +373,7 @@ static struct ast_node *parse_var_decl()
 
 static struct ast_node *parse_decl()
 {
-    const struct token *t = peek_current();
+    struct token *t = peek_current();
 
     switch (t->type) {
     case TOK_STRUCT:
@@ -398,8 +399,8 @@ static struct ast_node *parse_struct_decl()
 {
     ast_array_t decls = {0};
 
-    const struct token *start = require_token(TOK_STRUCT);
-    const struct token *name = require_token(TOK_SYMBOL);
+    struct token *start = require_token(TOK_STRUCT);
+    struct token *name  = require_token(TOK_SYMBOL);
 
     require_char('{');
 
@@ -454,7 +455,7 @@ static struct ast_node *parse_function_param_list()
 static struct ast_node *parse_function_decl()
 {
     struct localized_data_type dt = parse_return_type();
-    const struct token *name = require_token(TOK_SYMBOL);
+    struct token *name = require_token(TOK_SYMBOL);
 
     require_char('(');
     struct ast_node *param_list = parse_function_param_list();
@@ -479,7 +480,7 @@ static struct ast_node *parse_function_decl()
 
 static struct ast_node *parse_stmt()
 {
-    const struct token *t = peek_current();
+    struct token *t = peek_current();
 
     switch (t->type) {
     case TOK_OPEN_CURLY_BRACKET:
@@ -525,7 +526,7 @@ static struct ast_node *parse_stmt()
 
 static struct ast_node *parse_loop_stmt()
 {
-    const struct token *t = peek_next();
+    struct token *t = peek_next();
 
     switch (t->type) {
     case TOK_BREAK:
@@ -540,8 +541,8 @@ static struct ast_node *parse_loop_stmt()
 
 static struct ast_node *parse_iteration_block()
 {
-    ast_array_t stmts = {0};
-    const struct token *start = require_char('{');
+    ast_array_t   stmts = {0};
+    struct token *start = require_char('{');
 
     while (!tok_is(peek_current(), '}')) {
         vector_push_back(stmts, parse_loop_stmt());
@@ -583,8 +584,8 @@ static struct ast_node *parse_block()
     if (loops_depth > 0)
         return parse_iteration_block();
 
-    ast_array_t stmts = {0};
-    const struct token *start = require_char('{');
+    ast_array_t   stmts = {0};
+    struct token *start = require_char('{');
 
     while (!tok_is(peek_current(), '}')) {
         vector_push_back(stmts, parse_stmt());
@@ -621,10 +622,10 @@ static struct ast_node *parse_block()
 
 static struct ast_node *parse_selection_stmt()
 {
-    struct ast_node    *cond      = NULL;
-    struct ast_node    *then_body = NULL;
-    struct ast_node    *else_body = NULL;
-    const struct token *start     = require_token(TOK_IF);
+    struct ast_node *cond      = NULL;
+    struct ast_node *then_body = NULL;
+    struct ast_node *else_body = NULL;
+    struct token    *start     = require_token(TOK_IF);
 
     require_char('(');
     cond = parse_logical_or();
@@ -648,7 +649,7 @@ static struct ast_node *parse_selection_stmt()
 
 static struct ast_node *parse_iteration_stmt()
 {
-    const struct token *t = peek_current();
+    struct token *t = peek_current();
 
     switch (t->type) {
     case TOK_FOR:
@@ -664,7 +665,7 @@ static struct ast_node *parse_iteration_stmt()
 
 static struct ast_node *parse_jump_stmt()
 {
-    const struct token *start = require_token(TOK_RETURN);
+    struct token *start = require_token(TOK_RETURN);
     struct ast_node  *body  = NULL;
 
     if (!tok_is(peek_current(), ';'))
@@ -720,7 +721,7 @@ static struct ast_node *parse_for_range(
 /// absurd.
 static struct ast_node *parse_for()
 {
-    const struct token *start = require_token(TOK_FOR);
+    struct token *start = require_token(TOK_FOR);
     require_char('(');
 
     struct ast_node *init      = NULL;
@@ -730,7 +731,7 @@ static struct ast_node *parse_for()
     if (!tok_is(peek_next(), ';')) {
         --tok_begin;
 
-        const struct token *curr = peek_current();
+        struct token *curr = peek_current();
         struct localized_data_type dt = parse_type();
 
         (void) dt;
@@ -778,7 +779,7 @@ static struct ast_node *parse_for()
 
 static struct ast_node *parse_do_while()
 {
-    const struct token *start = require_token(TOK_DO);
+    struct token *start = require_token(TOK_DO);
 
     ++loops_depth;
     struct ast_node *body = parse_block();
@@ -800,7 +801,7 @@ static struct ast_node *parse_do_while()
 
 static struct ast_node *parse_while()
 {
-    const struct token *start = require_token(TOK_WHILE);
+    struct token *start = require_token(TOK_WHILE);
 
     require_char('(');
     struct ast_node *cond = parse_logical_or();
@@ -822,8 +823,8 @@ static struct ast_node *parse_logical_or()
 {
     struct ast_node *expr = parse_logical_and();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_OR:
             expr = ast_binary_init(t->type, expr, parse_logical_or(), t->line_no, t->col_no);
@@ -841,8 +842,8 @@ static struct ast_node *parse_logical_and()
 {
     struct ast_node *expr = parse_inclusive_or();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_AND:
             expr = ast_binary_init(t->type, expr, parse_logical_and(), t->line_no, t->col_no);
@@ -860,8 +861,8 @@ static struct ast_node *parse_inclusive_or()
 {
     struct ast_node *expr = parse_exclusive_or();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_BIT_OR:
             expr = ast_binary_init(t->type, expr, parse_inclusive_or(), t->line_no, t->col_no);
@@ -879,8 +880,8 @@ static struct ast_node *parse_exclusive_or()
 {
     struct ast_node *expr = parse_and();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_XOR:
             expr = ast_binary_init(t->type, expr, parse_exclusive_or(), t->line_no, t->col_no);
@@ -898,8 +899,8 @@ static struct ast_node *parse_and()
 {
     struct ast_node *expr = parse_equality();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_BIT_AND:
             expr = ast_binary_init(t->type, expr, parse_and(), t->line_no, t->col_no);
@@ -917,8 +918,8 @@ static struct ast_node *parse_equality()
 {
     struct ast_node *expr = parse_relational();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_EQ:
         case TOK_NEQ: /// Fall through.
@@ -937,8 +938,8 @@ static struct ast_node *parse_relational()
 {
     struct ast_node *expr = parse_shift();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_GT:
         case TOK_LT:
@@ -959,8 +960,8 @@ static struct ast_node *parse_shift()
 {
     struct ast_node *expr = parse_additive();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_SHL:
         case TOK_SHR: /// Fall through.
@@ -979,8 +980,8 @@ static struct ast_node *parse_additive()
 {
     struct ast_node *expr = parse_multiplicative();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_PLUS:
         case TOK_MINUS: /// Fall through.
@@ -999,8 +1000,8 @@ static struct ast_node *parse_multiplicative()
 {
     struct ast_node *expr = parse_prefix_unary();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
         switch (t->type) {
         case TOK_STAR:
         case TOK_SLASH:
@@ -1018,7 +1019,7 @@ static struct ast_node *parse_multiplicative()
 
 static struct ast_node *parse_prefix_unary()
 {
-   const struct token *t = peek_next();
+    struct token *t = peek_next();
 
     switch (t->type) {
     case TOK_BIT_AND: /// Address operator `&`.
@@ -1041,8 +1042,8 @@ static struct ast_node *parse_prefix_unary()
 
 static struct ast_node *parse_postfix_unary()
 {
-    struct ast_node  *expr = parse_primary();
-    const struct token *t    = peek_next();
+    struct ast_node *expr = parse_primary();
+    struct token    *t    = peek_next();
 
     switch (t->type) {
     case TOK_INC:
@@ -1062,8 +1063,8 @@ static struct ast_node *parse_postfix_unary()
 
 static struct ast_node *parse_symbol()
 {
-    const struct token *start    = tok_begin - 1;
-    const struct token *curr_tok = tok_begin;
+    struct token *start    = tok_begin - 1;
+    struct token *curr_tok = tok_begin;
 
     switch (curr_tok->type) {
     /// symbol(
@@ -1086,7 +1087,7 @@ static struct ast_node *parse_symbol()
 
 static struct ast_node *parse_primary()
 {
-    const struct token *t = peek_next();
+    struct token *t = peek_next();
 
     switch (t->type) {
     case TOK_SYMBOL:
@@ -1113,9 +1114,10 @@ static struct ast_node *parse_primary()
 
 static struct ast_node *parse_struct_var_decl()
 {
-    struct localized_data_type dt = parse_type();
-    const struct token *name = require_token(TOK_SYMBOL);
-    ast_array_t enclosure_list = {0};
+    struct localized_data_type
+                  dt             = parse_type();
+    struct token *name           = require_token(TOK_SYMBOL);
+    ast_array_t   enclosure_list = {0};
 
     assert(dt.data_type == D_T_STRUCT);
 
@@ -1166,8 +1168,8 @@ static struct ast_node *parse_struct_var_decl()
 
 static struct ast_node *parse_struct_field_access()
 {
-    const struct token *symbol = require_token(TOK_SYMBOL);
-    const struct token *next = peek_next();
+    struct token *symbol = require_token(TOK_SYMBOL);
+    struct token *next   = peek_next();
 
     if (tok_is(next, '.'))
         return ast_member_init(
@@ -1183,7 +1185,7 @@ static struct ast_node *parse_struct_field_access()
 
 static struct ast_node *parse_array_access()
 {
-    const struct token *symbol = peek_next();
+    struct token *symbol = peek_next();
 
     if (!tok_is(peek_current(), '['))
         weak_compile_error(
@@ -1217,7 +1219,7 @@ static struct ast_node *parse_array_access()
 
 static struct ast_node *parse_expr()
 {
-    const struct token *t = peek_current();
+    struct token *t = peek_current();
 
     switch (t->type) {
     case TOK_INT:
@@ -1234,8 +1236,8 @@ static struct ast_node *parse_assignment()
 {
     struct ast_node *expr = parse_logical_or();
 
-    while (true) {
-        const struct token *t = peek_next();
+    while (1) {
+        struct token *t = peek_next();
 
         switch (t->type) {
         case TOK_ASSIGN:
@@ -1269,7 +1271,7 @@ static struct ast_node *parse_assignment()
 
 static struct ast_node *parse_function_call()
 {
-    const struct token *name = peek_next();
+    struct token *name = peek_next();
 
     ast_array_t args_list = {0};
 
@@ -1314,7 +1316,7 @@ static struct ast_node *parse_function_call()
 
 static struct ast_node *parse_constant()
 {
-    const struct token *t = peek_next();
+    struct token *t = peek_next();
 
     switch (t->type) {
     case TOK_INTEGRAL_LITERAL:
@@ -1337,3 +1339,14 @@ static struct ast_node *parse_constant()
         );
     }
 }
+
+/*
+Однажды болезнями, стонами, страхами затаёнными
+Ты придёшь на голос мой - я позову.
+Тропы родными протоптаны,
+Мрамор высечен, ямы закопаны.
+С головой под землёй в нижнем ряду.
+Горя слезами невечными,
+Зеркалами завешанными
+Ты пойдёшь вслед за мной - я провожу.
+*/
