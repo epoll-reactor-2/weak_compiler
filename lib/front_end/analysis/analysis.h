60b57fc75326a03e4aff407b410aad44d1151621
epoll-reactor-2 <>
2023-10-13 17:08:39 +0200

    Supplement documentation.

--- lib/front_end/analysis/analysis.h
+++ lib/front_end/analysis/analysis.h
@@ -19,11 +19,24 @@ void analysis_variable_use_analysis(struct ast_node *root);
 
 /// \brief Function analyzer.
 ///
+/// Asserts listed below conditions.
+///
 /// \note Should be called after variable use analysis.
 ///
-/// Asserts listed below conditions.
-///   - Arguments size and types matches called function prototype.
-///   - Return value matches its function return type.
+/// <table>
+///   <tr>
+///     <th>Expression</th>
+///     <th>Semantic</th>
+///   </tr>
+///   <tr>
+///     <th>int f() { return 1; }</th>
+///     <td>Return value is of function return type.</td>
+///   </tr>
+///   <tr>
+///     <th>void f(int arg) {}, f(0)</th>
+///     <td>Function argument is of type in signature.</td>
+///   </tr>
+/// </table>
 void analysis_functions_analysis(struct ast_node *root);
 
 /// \brief Type checker.
@@ -42,27 +55,22 @@ void analysis_functions_analysis(struct ast_node *root);
 ///     <td>Same integral or pointer types (int, char, float, bool).</td>
 ///   </tr>
 ///   <tr>
+///     <th>x <op> y</th>
+///     <td>Allowed operator applied to the operands.</td>
+///   </tr>
+///   <tr>
 ///     <th>++x</th>
 ///     <td>Int or char operand type.</td>
-///   </tr>
-///   <tr>
-///     <th>int f() { return 1; }</th>
-///     <td>Return value is of function return type.</td>
-///   </tr>
-///   <tr>
-///     <th>void f(int arg) {}, f(0)</th>
-///     <td>Function argument is of type in signature.</td>
-///   </tr>
-///   <tr>
-///     <th>mem[1] | mem[var]</th>
-///     <td>Integer as array index.</td>
 ///   </tr>
 ///   <tr>
 ///     <th>*x</th>
 ///     <td>Pointer type with indirection level >= 1.</td>
 ///   </tr>
+///   <tr>
+///     <th>mem[1] | mem[var]</th>
+///     <td>Integer as array index.</td>
+///   </tr>
 /// </table>
-
 void analysis_type_analysis(struct ast_node *root);
 
 #endif // WEAK_COMPILER_FRONTEND_ANALYSIS_VARIABLE_USAGE_H
