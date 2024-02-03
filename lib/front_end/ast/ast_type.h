bae1a95e15972265ba6218f64a3142157c62751f
epoll-reactor-2 <>
2024-02-03 14:33:22 +0000

    Implicit cast AST.

--- lib/front_end/ast/ast_type.h
+++ lib/front_end/ast/ast_type.h
@@ -8,50 +8,53 @@
 #define WEAK_COMPILER_FRONTEND_AST_AST_TYPE_H
 
 enum ast_type {
-  /** Literals. */
-  AST_CHAR_LITERAL,
-  AST_INTEGER_LITERAL,
-  AST_FLOATING_POINT_LITERAL,
-  AST_STRING_LITERAL,
-  AST_BOOLEAN_LITERAL,
+    /** Literals. */
+    AST_CHAR_LITERAL,
+    AST_INTEGER_LITERAL,
+    AST_FLOATING_POINT_LITERAL,
+    AST_STRING_LITERAL,
+    AST_BOOLEAN_LITERAL,
 
-  /** Variable reference. */
-  AST_SYMBOL,
+    /** Variable reference. */
+    AST_SYMBOL,
 
-  // Declarations.
-  AST_VAR_DECL,
-  AST_ARRAY_DECL,
-  AST_STRUCT_DECL,
+    // Declarations.
+    AST_VAR_DECL,
+    AST_ARRAY_DECL,
+    AST_STRUCT_DECL,
 
-  /** Iteration statements. */
-  AST_BREAK_STMT,
-  AST_CONTINUE_STMT,
+    /** Iteration statements. */
+    AST_BREAK_STMT,
+    AST_CONTINUE_STMT,
 
-  /** Operators. */
-  AST_BINARY,
-  AST_PREFIX_UNARY,
-  AST_POSTFIX_UNARY,
-  AST_ARRAY_ACCESS,
-  AST_MEMBER,
+    /** Operators. */
+    AST_BINARY,
+    AST_PREFIX_UNARY,
+    AST_POSTFIX_UNARY,
+    AST_ARRAY_ACCESS,
+    AST_MEMBER,
 
-  /** Selection statements. */
-  AST_IF_STMT,
+    /** Selection statements. */
+    AST_IF_STMT,
 
-  /** Loops. */
-  AST_FOR_STMT,
-  AST_FOR_RANGE_STMT,
-  AST_WHILE_STMT,
-  AST_DO_WHILE_STMT,
+    /** Loops. */
+    AST_FOR_STMT,
+    AST_FOR_RANGE_STMT,
+    AST_WHILE_STMT,
+    AST_DO_WHILE_STMT,
 
-  /** Jump statements. */
-  AST_RETURN_STMT,
+    /** Jump statements. */
+    AST_RETURN_STMT,
 
-  /** Block statements. */
-  AST_COMPOUND_STMT,
+    /** Block statements. */
+    AST_COMPOUND_STMT,
 
-  /** Functions. */
-  AST_FUNCTION_DECL,
-  AST_FUNCTION_CALL
+    /** Functions. */
+    AST_FUNCTION_DECL,
+    AST_FUNCTION_CALL,
+
+    /** Implicit type conversion. */
+    AST_IMPLICIT_CAST
 };
 
 /** \return String representation of the AST type. Don't
