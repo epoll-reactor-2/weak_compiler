8dbb7caf16275e8cdd11f8f2ee841a96e14ec646
epoll-reactor-2 <>
2024-03-11 18:46:17 +0100

    Note instruction encoding in binary format.

--- lib/back_end/risc_v.h
+++ lib/back_end/risc_v.h
@@ -17,54 +17,56 @@
  **         Instruction encoding             **
  **********************************************/
 /* R type */
-#define risc_v_R_add                51 /* 0b110011 + (0 << 12) */
-#define risc_v_R_sub        1073741875 /* 0b110011 + (0 << 12) + (0x20 << 25) */
-#define risc_v_R_xor             16435 /* 0b110011 + (4 << 12) */
-#define risc_v_R_or              24627 /* 0b110011 + (6 << 12) */
-#define risc_v_R_and             28723 /* 0b110011 + (7 << 12) */
-#define risc_v_R_sll              4147 /* 0b110011 + (1 << 12) */
-#define risc_v_R_srl             20531 /* 0b110011 + (5 << 12) */
-#define risc_v_R_sra        1073762355 /* 0b110011 + (5 << 12) + (0x20 << 25) */
-#define risc_v_R_slt              8243 /* 0b110011 + (2 << 12) */
-#define risc_v_R_sltu            12339 /* 0b110011 + (3 << 12) */
+#define risc_v_R_add                (0b110011 + (0 << 12))
+#define risc_v_R_sub                (0b110011 + (0 << 12) + (0x20 << 25))
+#define risc_v_R_xor                (0b110011 + (4 << 12))
+#define risc_v_R_or                 (0b110011 + (6 << 12))
+#define risc_v_R_and                (0b110011 + (7 << 12))
+#define risc_v_R_sll                (0b110011 + (1 << 12))
+#define risc_v_R_srl                (0b110011 + (5 << 12))
+#define risc_v_R_sra                (0b110011 + (5 << 12) + (0x20 << 25))
+#define risc_v_R_slt                (0b110011 + (2 << 12))
+#define risc_v_R_sltu               (0b110011 + (3 << 12))
 /* I type */
-#define risc_v_I_addi               19 /* 0b0010011 */
-#define risc_v_I_xori            16403 /* 0b0010011 + (4 << 12) */
-#define risc_v_I_ori             24595 /* 0b0010011 + (6 << 12) */
-#define risc_v_I_andi            28691 /* 0b0010011 + (7 << 12) */
-#define risc_v_I_slli             4115 /* 0b0010011 + (1 << 12) */
-#define risc_v_I_srli            20499 /* 0b0010011 + (5 << 12) */
-#define risc_v_I_srai       1073762323 /* 0b0010011 + (5 << 12) + (0x20 << 25) */
-#define risc_v_I_slti             8211 /* 0b0010011 + (2 << 12) */
-#define risc_v_I_sltiu           12307 /* 0b0010011 + (3 << 12) */
+#define risc_v_I_addi               (0b0010011)
+#define risc_v_I_xori               (0b0010011 + (4 << 12))
+#define risc_v_I_ori                (0b0010011 + (6 << 12))
+#define risc_v_I_andi               (0b0010011 + (7 << 12))
+#define risc_v_I_slli               (0b0010011 + (1 << 12))
+#define risc_v_I_srli               (0b0010011 + (5 << 12))
+#define risc_v_I_srai               (0b0010011 + (5 << 12) + (0x20 << 25))
+#define risc_v_I_slti               (0b0010011 + (2 << 12))
+#define risc_v_I_sltiu              (0b0010011 + (3 << 12))
 /* Load/store */
-#define risc_v_I_lb                  3 /* 0b11 */
-#define risc_v_I_lh               4099 /* 0b11 + (1 << 12) */
-#define risc_v_I_lw               8195 /* 0b11 + (2 << 12) */
-#define risc_v_I_lbu             16387 /* 0b11 + (4 << 12) */
-#define risc_v_I_lhu             20483 /* 0b11 + (5 << 12) */
-#define risc_v_S_sb                 35 /* 0b0100011 */
-#define risc_v_S_sh               4131 /* 0b0100011 + (1 << 12) */
-#define risc_v_S_sw               8227 /* 0b0100011 + (2 << 12) */
+#define risc_v_I_lb                 (0b11                 )
+#define risc_v_I_lh                 (0b11 + (1 << 12)     )
+#define risc_v_I_lw                 (0b11 + (2 << 12)     )
+#define risc_v_I_ld                 (0b11 + (3 << 12)     )
+#define risc_v_I_lbu                (0b11 + (4 << 12)     )
+#define risc_v_I_lhu                (0b11 + (5 << 12)     )
+#define risc_v_S_sb                 (0b0100011            )
+#define risc_v_S_sh                 (0b0100011 + (1 << 12))
+#define risc_v_S_sw                 (0b0100011 + (2 << 12))
+#define risc_v_S_sd                 (0b0100011 + (3 << 12))
 /* Branches */
-#define risc_v_B_beq                99 /* 0b1100011 */
-#define risc_v_B_bne              4195 /* 0b1100011 + (1 << 12) */
-#define risc_v_B_blt             16483 /* 0b1100011 + (4 << 12) */
-#define risc_v_B_bge             20579 /* 0b1100011 + (5 << 12) */
-#define risc_v_B_bltu            24675 /* 0b1100011 + (6 << 12) */
-#define risc_v_B_bgeu            28771 /* 0b1100011 + (7 << 12) */
+#define risc_v_B_beq                (0b1100011            )
+#define risc_v_B_bne                (0b1100011 + (1 << 12))
+#define risc_v_B_blt                (0b1100011 + (4 << 12))
+#define risc_v_B_bge                (0b1100011 + (5 << 12))
+#define risc_v_B_bltu               (0b1100011 + (6 << 12))
+#define risc_v_B_bgeu               (0b1100011 + (7 << 12))
 /* Jumps */
-#define risc_v_I_jal               111 /* 0b1101111 */
-#define risc_v_I_jalr              103 /* 0b1100111 */
+#define risc_v_I_jal                (0b1101111)
+#define risc_v_I_jalr               (0b1100111)
 /* Misc */
-#define risc_v_I_lui                55 /* 0b0110111 */
-#define risc_v_I_auipc              23 /* 0b0010111 */
-#define risc_v_I_ecall             115 /* 0b1110011 */
-#define risc_v_I_ebreak        1048691 /* 0b1110011 + (1 << 20) */
+#define risc_v_I_lui                (0b0110111)
+#define risc_v_I_auipc              (0b0010111)
+#define risc_v_I_ecall              (0b1110011)
+#define risc_v_I_ebreak             (0b1110011 + (1 << 20))
 /* M */
-#define risc_v_M_mul          33554483 /* 0b0110011 + (1 << 25) */
-#define risc_v_M_div          33570867 /* 0b0110011 + (1 << 25) + (4 << 12) */
-#define risc_v_M_mod          33579059 /* 0b0110011 + (1 << 25) + (6 << 12) */
+#define risc_v_M_mul                (0b0110011 + (1 << 25))
+#define risc_v_M_div                (0b0110011 + (1 << 25) + (4 << 12))
+#define risc_v_M_mod                (0b0110011 + (1 << 25) + (6 << 12))
 
 /* Registers */
 #define risc_v_reg_zero    0
