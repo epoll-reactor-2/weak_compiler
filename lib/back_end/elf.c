0daf09113ec739992e0950bb6ea25a408579471a
epoll-reactor-2 <>
2024-06-17 09:58:22 +0200

    Use defines in ELF generator.

--- lib/back_end/elf.c
+++ lib/back_end/elf.c
@@ -16,33 +16,32 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
-static char *map;
-static int fd;
-
-static int phdr_off       = 0x0040;
-static int sh_size        = 0x0040;
-static int phdr_size      = 0x0038;
-
-static int init_size      = 0x8000;
-static int strtab_off     = 0x1000;
-static int shstrtab_off   = 0x1500;
-static int symtab_off     = 0x2000;
-static int sh_off         = 0x4000;
-static int text_off       = 0x6000;
-static int entry_addr     = 0x41000;
+#define ELF_PHDR_OFF                0x0040
+#define ELF_SH_SIZE                 0x0040
+#define ELF_PHDR_SIZE               0x0038
+#define ELF_INIT_SIZE               0x8000
+#define ELF_STRTAB_OFF              0x1000
+#define ELF_SHSTRTAB_OFF            0x1500
+#define ELF_SYMTAB_OFF              0x2000
+#define ELF_SH_OFF                  0x4000
+#define ELF_TEXT_OFF                0x6000
+#define ELF_ENTRY_ADDR              0x41000
 /* How much bytes occupy one symtab entry. */
-static int symtab_entsize = 24;
+#define ELF_SYMTAB_ENTSIZE          24
+
+#define ELF64_ST_BIND(info)         ((info) >> 4)
+#define ELF64_ST_TYPE(info)         ((info) & 0xf)
+#define ELF64_ST_INFO(bind, type)   (((bind)<<4)+((type)&0xf))
 
 static int arch         = 0x00;
 static int text_size    = 0x00;
 static int syms_cnt     = 0x00;
 static int strtab_size  = 0x00;
 
-struct codegen_output *codegen_output;
+static char *map;
+static int fd;
 
-#define ELF64_ST_BIND(info)          ((info) >> 4)
-#define ELF64_ST_TYPE(info)          ((info) & 0xf)
-#define ELF64_ST_INFO(bind, type)    (((bind)<<4)+((type)&0xf))
+struct codegen_output *codegen_output;
 
 #define emit(addr, byte_string) \
     { strcpy(&map[addr], byte_string); }
@@ -61,14 +60,14 @@ static char *emit_symbol(char *start, const char *section)
 
 static void emit_phdr(uint64_t idx, struct elf_phdr *phdr)
 {
-    uint64_t off = phdr_off + (phdr_size * idx);
+    uint64_t off = ELF_PHDR_OFF + (ELF_PHDR_SIZE * idx);
 
     emit_bytes(off, phdr);
 }
 
 static void emit_shdr(uint64_t idx, struct elf_shdr *shdr)
 {
-    uint64_t shdr_off = sh_off + (sh_size * idx);
+    uint64_t shdr_off = ELF_SH_OFF + (ELF_SH_SIZE * idx);
 
     emit_bytes(shdr_off, shdr);
 }
@@ -82,9 +81,9 @@ static uint16_t emit_phdrs()
     struct elf_phdr phdr = {
         .type   = 0x01,
         .flags  = 7,
-        .off    = text_off,
-        .vaddr  = entry_addr,
-        .paddr  = entry_addr,
+        .off    = ELF_TEXT_OFF,
+        .vaddr  = ELF_ENTRY_ADDR,
+        .paddr  = ELF_ENTRY_ADDR,
         .memsz  = text_size,
         .filesz = text_size,
         .align  = 0x1
@@ -113,8 +112,8 @@ static uint16_t emit_shdrs()
     struct elf_shdr progbits_shdr = {
         .name_ptr = SYMTAB_NULL_END,
         .type     = SHT_PROGBITS,
-        .addr     = entry_addr,
-        .off      = text_off,
+        .addr     = ELF_ENTRY_ADDR,
+        .off      = ELF_TEXT_OFF,
         .size     = text_size,
         .flags    = SHF_ALLOC | SHF_EXECINSTR,
         .link     = 0x00
@@ -124,8 +123,8 @@ static uint16_t emit_shdrs()
     struct elf_shdr strtab_shdr = {
         .name_ptr = SYMTAB_TEXT_SYM_END,
         .type     = SHT_STRTAB,
-        .addr     = strtab_off,
-        .off      = strtab_off,
+        .addr     = ELF_STRTAB_OFF,
+        .off      = ELF_STRTAB_OFF,
         .size     = strtab_size + /* NULL entry. */ 1,
         .link     = 0x00
     };
@@ -134,8 +133,8 @@ static uint16_t emit_shdrs()
     struct elf_shdr shstrtab_shdr = {
         .name_ptr = SYMTAB_STRTAB_SYM_END,
         .type     = SHT_STRTAB,
-        .addr     = shstrtab_off,
-        .off      = shstrtab_off,
+        .addr     = ELF_SHSTRTAB_OFF,
+        .off      = ELF_SHSTRTAB_OFF,
         .size     = 0x100,
         .link     = 0x00
     };
@@ -144,11 +143,11 @@ static uint16_t emit_shdrs()
     struct elf_shdr symtab_shdr = {
         .name_ptr = SYMTAB_SHSTRTAB_SYM_END,
         .type     = SHT_SYMTAB,
-        .addr     = symtab_off,
-        .off      = symtab_off,
-        .size     = syms_cnt * symtab_entsize,
+        .addr     = ELF_SYMTAB_OFF,
+        .off      = ELF_SYMTAB_OFF,
+        .size     = syms_cnt * ELF_SYMTAB_ENTSIZE,
         .info     = syms_cnt,
-        .entsize  = symtab_entsize,
+        .entsize  = ELF_SYMTAB_ENTSIZE,
         .link     = 0x02
     };
     emit_shdr(shnum++, &symtab_shdr);
@@ -187,7 +186,7 @@ unused static char *emit_symtab_entry(
         .shndx = sentinel ? 0 : 1,
         .info  = sentinel ? 0 : 0
     };
-    emit_bytes(symtab_off + *sym_it, &sym);
+    emit_bytes(ELF_SYMTAB_OFF + *sym_it, &sym);
 
     /* This thing is wierd as fuck. I don't get `info`
        field at proper layout when I follow official ELF manual,
@@ -196,15 +195,15 @@ unused static char *emit_symtab_entry(
         /* TODO: Index to some section. */
         uint8_t byte = 0x01;
         /* TODO: Figure out why ndx is on -18. */
-        emit_bytes(symtab_off + *sym_it + 6, &byte);
+        emit_bytes(ELF_SYMTAB_OFF + *sym_it + 6, &byte);
         byte = ELF64_ST_INFO(2, 2);
         /* TODO: Figure out why type is on -20. */
-        emit_bytes(symtab_off + *sym_it + 4, &byte);
+        emit_bytes(ELF_SYMTAB_OFF + *sym_it + 4, &byte);
 
         *str_it += strlen(name) + /* NULL */ 1;
     }
 
-    *sym_it += symtab_entsize;
+    *sym_it += ELF_SYMTAB_ENTSIZE;
 
     ++syms_cnt;
     return s;
@@ -217,14 +216,14 @@ static void emit_elf()
         .type      = ET_EXEC,
         .machine   = arch,
         .version   = 1,
-        .entry     = entry_addr,
-        .phoff     = phdr_off,
-        .shoff     = sh_off,
+        .entry     = ELF_ENTRY_ADDR,
+        .phoff     = ELF_PHDR_OFF,
+        .shoff     = ELF_SH_OFF,
         .flags     = 0x00,
         .ehsize    = 0x40,
-        .phentsize = phdr_size,
+        .phentsize = ELF_PHDR_SIZE,
         .phnum     = emit_phdrs(),
-        .shentsize = sh_size,
+        .shentsize = ELF_SH_SIZE,
         .shnum     = emit_shdrs(),
         .shstrndx  = 0x02
     };
@@ -234,14 +233,14 @@ static void emit_elf()
 
 static void elf_put_code()
 {
-    void *start = map + text_off;
+    void *start = map + ELF_TEXT_OFF;
     memcpy(start,
         codegen_output->instrs.data,
         codegen_output->instrs.size
     );
 
     /* +1 is due to first NULL byte for NULL section. */
-    char *s = &map[strtab_off + 1];
+    char *s = &map[ELF_STRTAB_OFF + 1];
     s = emit_symbol(s, ".text");
     s = emit_symbol(s, ".strtab");
     s = emit_symbol(s, ".shstrtab");
@@ -251,12 +250,12 @@ static void elf_put_code()
     uint64_t sym_it = 0;
 
     /* Sentinel NULL entry is necessary. */
-    s = emit_symtab_entry(&str_it, &sym_it, s, entry_addr, NULL);
+    s = emit_symtab_entry(&str_it, &sym_it, s, ELF_ENTRY_ADDR, NULL);
     hashmap_foreach(&codegen_output->fn_offsets, k, v) {
         const char *name = (const char *) k;
         uint64_t    off  = v;
 
-        s = emit_symtab_entry(&str_it, &sym_it, s, entry_addr + off, name);
+        s = emit_symtab_entry(&str_it, &sym_it, s, ELF_ENTRY_ADDR + off, name);
     }
 
     text_size = codegen_output->instrs.count;
@@ -274,10 +273,10 @@ void elf_init(struct elf_entry *e)
 {
     fd = open(e->filename, O_CREAT | O_TRUNC | O_RDWR, 0666);
 
-    if (ftruncate(fd, init_size) < 0)
+    if (ftruncate(fd, ELF_INIT_SIZE) < 0)
         weak_fatal_errno("ftruncate()");
 
-    map = (char *) mmap(NULL, init_size, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+    map = (char *) mmap(NULL, ELF_INIT_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
     if ((void *) map == MAP_FAILED)
         weak_fatal_errno("mmap()");
 
@@ -291,7 +290,7 @@ void elf_init(struct elf_entry *e)
 
 void elf_exit()
 {
-    if (munmap(map, init_size) < 0)
+    if (munmap(map, ELF_INIT_SIZE) < 0)
         weak_fatal_errno("munmap()");
 
     if (close(fd) < 0)
